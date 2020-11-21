3633ff35b34a1f5087c0728dbddf2c4abbd95240
rongying <kohrongying@gmail.com>
2020-11-22 00:05:28 +0800

    Update styling and footer

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>LastPoop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
@@ -72,14 +72,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <a href="https://github.com/kohrongying/last-poop">Github Source Code</a>
       </footer>
     </div>
   )
